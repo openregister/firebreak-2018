5428e19b3a2e07586b0347e41eb393b77437aa00
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-01-11 13:04:24 +0000

    File upload fix

--- app/controllers/temporary_register_controller.rb
+++ app/controllers/temporary_register_controller.rb
@@ -49,22 +49,7 @@ class TemporaryRegisterController < ApplicationController
 
     session[:included_fields] = params[:included_fields]
 
-    s3Client = Aws::S3::Resource.new(region:'eu-west-1')
 
-    session[:included_fields].each do |field_name|
-      fields_location = session[:custom_fields].select{|f| f['field'] == field_name}.length == 1 ? session[:custom_fields] : @available_fields
-      field = fields_location.select{|f| f['field'] == field_name}.first
-      register = field_name == session[:register_name] ? session[:register_name] : '';
-      field_yaml = { "cardinality" => '1', "datatype" => field['datatype'], "field" => field['field'], "phase" => 'experiment', "register" => register, "text" => field['text']}.to_yaml
-
-      obj = s3Client.bucket('firebreak-register-data').object("field/#{ field['field'] }.yaml")
-      obj.put(body: field_yaml)
-
-    end
-
-    register_yaml = { "fields" => session[:included_fields], "phase" => 'experiment', "register" => session[:register_name], "registry" => "government-digital-service", "text" => session[:register_description] }.to_yaml
-    obj = s3Client.bucket('firebreak-register-data').object("register/#{ session[:register_name] }.yaml")
-    obj.put(body: register_yaml)
 
 
 
@@ -106,9 +91,9 @@ class TemporaryRegisterController < ApplicationController
   def save_upload_data
     file = params[:register_data]
 
-    s3Client = Aws::S3::Resource.new(region:'eu-west-1')
-    obj = s3Client.bucket('firebreak-register-data').object(file.original_filename)
-    obj.upload_file(file.path)
+    upload_register_data_to_s3(@s3Client, session[:unique_register_id], file)
+
+    session[:register_data] = { original_filename: file.original_filename, path: file.path }
 
     redirect_from('upload_data')
   end
@@ -123,11 +108,31 @@ class TemporaryRegisterController < ApplicationController
                            .select{|f| session[:included_fields].include?(f.item.value['field'])}
                            .map{|f| f.item.value}
     @included_fields.concat(custom_fields_for_view)
+    @uploaded_file = session[:register_data]['original_filename']
+    @upload_file_name = session[:register_data]['path']
+
+
 
     render "summary"
   end
 
   def create_register
+    register_id = session[:unique_register_id]
+
+    upload_field_definition_to_s3(@s3Client, register_id)
+    upload_register_defintion_to_s3(@s3Client, register_id)
+
+    lambda_client = Aws::Lambda::Client.new(region: 'eu-west-1')
+    lambda_payload = JSON.generate({register_name: session[:register_name], phase: "experiment", register_id: session[:unique_register_id], register_data_file: session[:register_data]['original_filename'], custodian: "John Smith"})
+
+    lambda_response = lambda_client.invoke({
+        function_name: 'tsv_to_rsf',
+        invocation_type: 'RequestResponse',
+        log_type: 'None',
+        payload: lambda_payload
+    })
+    result = JSON.parse(lambda_response.payload.string)
+
     redirect_from('create_register')
   end
 
@@ -175,9 +180,40 @@ class TemporaryRegisterController < ApplicationController
     @available_fields = @@registers_client.get_register('field', 'beta').get_records.select{
         |r| ['name', 'official-name', 'start-date', 'end-date'].include?(r.item.value['field'])
     }.map{|r| r.item.value}
+
+    @s3Client = Aws::S3::Resource.new(region:'eu-west-1');
+
+    unless (session.has_key?(:unique_register_id))
+      session[:unique_register_id] = Time.now.to_i.to_s
+    end
   end
 
   private
+
+  def upload_field_definition_to_s3(s3Client, register_id)
+    session[:included_fields].each do |field_name|
+      fields_location = session[:custom_fields].select{|f| f['field'] == field_name}.length == 1 ? session[:custom_fields] : @available_fields
+      field = fields_location.select{|f| f['field'] == field_name}.first
+      register = field_name == session[:register_name] ? session[:register_name] : '';
+      field_yaml = { "cardinality" => '1', "datatype" => field['datatype'], "field" => field['field'], "phase" => 'experiment', "register" => register, "text" => field['text']}.to_yaml
+
+      obj = s3Client.bucket('firebreak-register-data').object("#{ register_id }/field/#{ field['field'] }.yaml")
+      obj.put(body: field_yaml)
+    end
+  end
+
+  def upload_register_defintion_to_s3(s3Client, register_id)
+    register_yaml = { "fields" => session[:included_fields], "phase" => 'experiment', "register" => session[:register_name], "registry" => "government-digital-service", "text" => session[:register_description] }.to_yaml
+    obj = s3Client.bucket('firebreak-register-data').object("#{ register_id }/register/#{ session[:register_name] }.yaml")
+    obj.put(body: register_yaml)
+
+
+  end
+
+  def upload_register_data_to_s3(s3Client, register_id, file)
+    obj = s3Client.bucket('firebreak-register-data').object("#{ register_id }/data/#{ file.original_filename }")
+    obj.upload_file(file.path)
+  end
 
   def custom_fields_for_view
     session[:custom_fields].map{|cf| {'field' => cf['field'], 'text' => cf['text'], 'datatype' => cf['datatype']}}
